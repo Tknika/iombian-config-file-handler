616cf25f2a350ed884019aa3c31438808fe1aa4a
aitorcas <aitorcas23@gmail.com>
2024-03-16 15:41:43 +0100

    Fix previous commit mistake

--- src/main.py
+++ src/main.py
@@ -15,8 +15,8 @@ LOG_LEVEL = os.environ.get("LOG_LEVEL", logging.INFO)
 BUTTON_EVENTS_HOST = os.environ.get("BUTTON_EVENTS_HOST", "127.0.0.1")
 BUTTON_EVENTS_PORT = int(os.environ.get("BUTTON_EVENTS_PORT" , 5556))
 YAML_FILE_PATH = os.environ.get("YAML_FILE_PATH", "/app/parameters.yml")
-SHUTDOWN_EVENT_HOST = os.environ.get("SHUTDOWN_HOST", "127.0.0.1")
-SHUTDOWN_EVENT_PORT = int(os.environ.get("SHUTDOWN_PORT", 5558))
+SHUTDOWN_SERVICE_HOST = os.environ.get("SHUTDOWN_HOST", "127.0.0.1")
+SHUTDOWN_SERVICE_PORT = int(os.environ.get("SHUTDOWN_PORT", 5558))
 
 CONFIG_HOST = "0.0.0.0"
 RESET_COMMAND = "reboot"
@@ -42,7 +42,7 @@ def signal_handler(sig, frame):
 
 def config_update_callback():
     logger.info("Rebooting the system")
-    logger.debug(f"Sending {RESET_COMMAND} command to {SHUTDOWN_EVENT_HOST}:{SHUTDOWN_EVENT_PORT}")
+    logger.debug(f"Sending {RESET_COMMAND} command to {SHUTDOWN_SERVICE_HOST}:{SHUTDOWN_SERVICE_PORT}")
     comm_module.execute_command(RESET_COMMAND)
     stop()
 
@@ -70,7 +70,7 @@ if __name__ == "__main__":
         on_message_callback=button_event_callback, host=BUTTON_EVENTS_HOST, port=BUTTON_EVENTS_PORT)
     client.start()
 
-    comm_module = CommunicationModule(host=SHUTDOWN_EVENT_HOST, port=SHUTDOWN_EVENT_PORT)
+    comm_module = CommunicationModule(host=SHUTDOWN_SERVICE_HOST, port=SHUTDOWN_SERVICE_PORT)
     comm_module.start()
 
     signal.signal(signal.SIGINT, signal_handler)
